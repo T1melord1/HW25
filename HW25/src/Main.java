191a6d41398adedb6bd1c487f87988dc86130077
vladb <vlad.bobrenok.2001@gmail.com>
2024-03-19 17:51:45 +0300

    test

--- HW25/src/Main.java
+++ HW25/src/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите имя, возраст и рост человека: ");
+        Supplier<Human> humanSupplier = () -> {
+            System.out.println("");
+            return null;
+        };
+
+    }
+    public static String scanner(){
+        return new Scanner(System.in).nextLine();
     }
 }
